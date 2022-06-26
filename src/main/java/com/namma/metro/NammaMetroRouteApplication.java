faec9c816166880eb03f85de5fd06ce4f59c182a
arjunmetre <arjunprojectexecute@gmail.com>
2022-06-26 16:16:04 +0800

    add log for start app

--- src/main/java/com/namma/metro/NammaMetroRouteApplication.java
+++ src/main/java/com/namma/metro/NammaMetroRouteApplication.java
@@ -3,14 +3,18 @@ package com.namma.metro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author Arjun Metre
  * 
  */
 @SpringBootApplication
+@Slf4j
 public class NammaMetroRouteApplication {
 
 	public static void main(String[] args) {
+		log.info("start server at");
 		SpringApplication.run(NammaMetroRouteApplication.class, args);
 	}
 
